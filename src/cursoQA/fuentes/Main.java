ce43f966bfe72954d9c7e84257ae9de537df4f56
cesar <cesar@familia>
2018-06-10 11:49:20 -0500

    Se agrega saludo de vienvenida , se retira traza de parametros

--- src/cursoQA/fuentes/Main.java
+++ src/cursoQA/fuentes/Main.java
@@ -3,9 +3,9 @@ package cursoQA.fuentes;
 import java.util.ArrayList;
 
 /**
- * Esta es una herramienta para generacion de claves
- * automatica que usa parametros para controlar
- * la creacion y la seguridad de las claves.
+ * Esta es una herramienta para generacion de claves automatica que usa
+ * parametros para controlar la creacion y la seguridad de las claves.
+ * 
  * @author cesar
  *
  */
@@ -13,6 +13,10 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
+		/**
+		 * Entrada al programa BigBankey Saludo de bienvenida
+		 */
+		System.out.println(":::::::::WELCOME::::::::::");
 		System.out.println("::::::::BIGBANKEY:::::::::");
 		System.out.println(":::::Power By Cesar8a:::::");
 		System.out.println(":::::     |||||      :::::");
@@ -21,37 +25,31 @@ public class Main {
 		System.out.println("::::::::::::::::::::::::::");
 
 		Entrada validaciones = new Entrada();
-		//se valida no habian argumentos de entrada para generar clave por defecto
+		// se valida no habian argumentos de entrada para generar clave por defecto
 		if (args.length == 0) {
 			Clave clave = new Clave();
 			clave.setListaOpciones(clave.OPCIONES_DEFECTO);
 			System.out.println(clave.generarClave());
-		} 
-		else {
+		} else {
 
 			validaciones.setListaOpcionesIngresadas(args);
-			
-			for (String string : args) {
-				System.out.println(string);
-			}
-			
 			if (validaciones.validarOpcionesIngresadas()) {
 				Clave clave = new Clave();
 				clave.setListaOpciones(args);
-				
+
 				if (clave.mostrarCantidadClaves(args) <= 1) {
 					clave.generarClave();
 					System.out.println(clave.generarClave());
 				} else {
 					ArrayList<String> listaClaves;
-					listaClaves = clave.mostrarArregloClaves();					
+					listaClaves = clave.mostrarArregloClaves();
 					for (int i = 0; i < listaClaves.size(); i++) {
 						System.out.println(listaClaves.get(i));
 					}
 				}
 			} else {
 
-				System.out.println("Intentalo nuevamente o pide ayuda con ?");
+				System.out.println("Intentalo nuevamente o pide ayuda con -?");
 			}
 
 		}
